51f0408e489ac969564c10a6d249b6d1d19c8f16
mael-queau <mael.queau99@gmail.com>
2022-05-25 17:35:32 +0200

    Build

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,16 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("colors");
-const appPort =
-  (_a = process.env.API_PORT) !== null && _a !== void 0 ? _a : 3000;
+const appPort = (_a = process.env.API_PORT) !== null && _a !== void 0 ? _a : 3000;
 const app = (0, express_1.default)();
 app.get("/", (_req, res) => {
-  res.status(200).send("Hello World!");
+    res.status(200).send("Hello World!");
 });
 app.listen(appPort, () => {
-  console.log(`🚀 Api listening on port ${appPort}!`.bgMagenta.white.bold);
+    console.log(`🚀 Api listening on port ${appPort}!`.grey.bold);
 });
