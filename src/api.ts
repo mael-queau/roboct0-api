bd1ee68ed59a9e96bb55c384bc13108bbb84073c
mael-queau <mael.queau99@gmail.com>
2022-07-08 22:37:31 +0200

    API key check fic

--- src/api.ts
+++ src/api.ts
@@ -3,16 +3,16 @@ import { Router } from "express";
 const router = Router();
 export default router;
 
-import v1 from "./v1";
-router.use("/v1", v1);
-
 router.use((req, res, next) => {
   if (req.headers["r0_key"] !== process.env.R0_KEY) {
     res.status(401).json({
       success: false,
-      message: "Invalid r0_key header.",
+      message: "Invalid api key.",
     });
   } else {
     next();
   }
 });
+
+import v1 from "./v1";
+router.use("/v1", v1);
