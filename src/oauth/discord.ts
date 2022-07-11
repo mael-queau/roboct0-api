ba437330681b236f9266fedfd81662796230374e
mael-queau <mael.queau99@gmail.com>
2022-07-11 16:27:49 +0200

    Discord oauth review and commenting

--- src/oauth/discord.ts
+++ src/oauth/discord.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 import { z, ZodError } from "zod";
 import { createState, deleteState } from "./helper";
 
@@ -7,7 +7,10 @@ export const router = Router();
 const prisma = new PrismaClient();
 
 router.get("/discord", async (_req, res) => {
+  // Create a state token to prevent CSRF.
   const state = await createState();
+
+  // Discord bot permissions
   const permissions = "309237902400";
   const scopes = ["bot", "applications.commands"];
 
@@ -23,15 +26,15 @@ router.get("/discord", async (_req, res) => {
 });
 
 router.get("/discord/callback", async (req, res) => {
-  const queryValidator = z.object({
-    code: z.string(),
-    guild_id: z.string(),
-    state: z.string(),
-  });
-
   try {
+    const queryValidator = z.object({
+      code: z.string(),
+      guild_id: z.string(),
+      state: z.string(),
+    });
     const parsedQuery = queryValidator.parse(req.query);
 
+    // Check that the state token matches so that we can prevent CSRF.
     const state = await prisma.state.findUnique({
       where: {
         value: parsedQuery.state,
@@ -39,15 +42,19 @@ router.get("/discord/callback", async (req, res) => {
     });
 
     if (state === null) {
+      // State token is invalid.
       res.status(401).json({
         success: false,
         message: "The 'state' query parameter is invalid.",
       });
     } else {
+      // Exchange the grant code for an access token.
       const { access_token, refresh_token } = await getAccessToken(
         parsedQuery.code
       );
 
+      // Create a new guild in the database.
+      // If the guild is already registered, update its access token and activate it.
       await prisma.guild.upsert({
         create: {
           guildId: parsedQuery.guild_id,
@@ -64,6 +71,7 @@ router.get("/discord/callback", async (req, res) => {
         },
       });
 
+      // Delete the state token.
       await deleteState(state);
 
       console.log(
@@ -76,20 +84,24 @@ router.get("/discord/callback", async (req, res) => {
       });
     }
   } catch (e) {
-    console.error(e);
     if (e instanceof ZodError) {
       if (req.query.error) {
+        // There was an error from Discord instead of a code.
         res.status(401).json({
           success: false,
           message: req.query.error_description,
         });
       } else {
+        // There was some other error with the query parameters.
         res.status(400).json({
           success: false,
           message: "The query parameters are invalid.",
         });
       }
     } else {
+      // There was some unknown error.
+      // Log it and send a generic error message.
+      console.error(e);
       res.status(500).json({
         success: false,
         message: "Internal server error.",
